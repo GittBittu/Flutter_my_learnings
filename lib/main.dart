c67eb9194ef23dcd241ac389317f3ecd4ba34195
GittBittu <Budhaditya.Nayak@outlook.com>
2023-08-10 21:17:17 +0530

    added buttom navigation bar and blank profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tesapp/pages/home_page.dart';
+import 'package:tesapp/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const WidgetTree(),
     );
   }
 }
