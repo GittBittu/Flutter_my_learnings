2540d98d2d44f4a6df3621d9d528793b2b90067c
GittBittu <Budhaditya.Nayak@outlook.com>
2023-08-11 21:39:47 +0530

    profile page is completed

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:tesapp/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Profile',
+        ),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 70,
+          ),
+          SizedBox(height: kDouble20),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Bittu'),
+          ),
+          ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('abd@xyz.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text('example.com'),
+          )
+        ],
+      ),
+    );
   }
 }
